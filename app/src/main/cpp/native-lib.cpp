64adbb97be3eea3486839dd66f9e3e6c92c7e70e
xkkjiayou <602409866@qq.com>
2018-12-03 17:11:16 +0800

    新增opencv Jni开发

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/opencv.hpp>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_findai_xkk_wearglass_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C+撒大声地+";
+
     return env->NewStringUTF(hello.c_str());
 }
