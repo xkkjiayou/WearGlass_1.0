aaf272bc6851165cba8a08cac74746685813145f
xkkjiayou <602409866@qq.com>
2018-12-06 17:45:30 +0800

    新增opencv Jni开发

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,6 @@
 #include <jni.h>
 #include <string>
-#include <opencv2/opencv.hpp>
+//#include <opencv2/opencv.hpp>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_findai_xkk_wearglass_MainActivity_stringFromJNI(
@@ -10,3 +10,77 @@ Java_com_findai_xkk_wearglass_MainActivity_stringFromJNI(
 
     return env->NewStringUTF(hello.c_str());
 }
+//
+//
+//#include <jni.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//using namespace cv;
+//using namespace std;
+//IplImage * change4channelTo3InIplImage(IplImage * src);
+//
+//extern "C" {
+//JNIEXPORT jstring JNICALL Java_com_findai_xkk_wearglass_MainActivity_stringFromJNI(
+//        JNIEnv* env, jobject obj, jintArray buf, int w, int h);
+//JNIEXPORT jstring JNICALL Java_com_findai_xkk_wearglass_MainActivity_stringFromJNI(
+//        JNIEnv* env, jobject obj, jintArray buf, int w, int h) {
+//
+//    jint *cbuf;
+//    cbuf = env->GetIntArrayElements(buf, false);
+//    if (cbuf == NULL) {
+//        return 0;
+//    }
+//
+//    Mat myimg(h, w, CV_8UC4, (unsigned char*) cbuf);
+//    Mat dst;
+//    int width = static_cast<float>(myimg.cols*0.5);
+//    //定义想要扩大或者缩小后的宽度，src.cols为原图像的宽度，乘以80%则得到想要的大小，并强制转换成float型
+//    int height = static_cast<float>(myimg.rows*0.5);
+//    //定义想要扩大或者缩小后的高度，src.cols为原图像的高度，乘以80%则得到想要的大小，并强制转换成float型
+//    cout<<dst;
+//    resize(myimg, dst, cv::Size(width, height));
+////    for(int i=0;i<dst.cols;i++){
+////        for(int j=0;j<dst.rows;j++){
+////            printf(dst[j][i])
+////        }
+////    }
+////    IplImage image=IplImage(myimg);
+////    IplImage* image3channel = change4channelTo3InIplImage(&image);
+////
+////    IplImage* pCannyImage=cvCreateImage(cvGetSize(image3channel),IPL_DEPTH_8U,1);
+////
+////    cvCanny(image3channel,pCannyImage,50,150,3);
+////
+////    int* outImage=new int[w*h];
+////    for(int i=0;i<w*h;i++)
+////    {
+////        outImage[i]=(int)pCannyImage->imageData[i];
+////    }
+////
+////    int size = w * h;
+////    jintArray result = env->NewIntArray(size);
+////    env->SetIntArrayRegion(result, 0, size, outImage);
+////    env->ReleaseIntArrayElements(buf, cbuf, 0);
+//    std::string hello = "Hello from C+撒大声地+";
+////
+//    return env->NewStringUTF(hello.c_str());
+//}
+//}
+//
+//IplImage * change4channelTo3InIplImage(IplImage * src) {
+//    if (src->nChannels != 4) {
+//        return NULL;
+//    }
+//
+//    IplImage * destImg = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);
+//    for (int row = 0; row < src->height; row++) {
+//        for (int col = 0; col < src->width; col++) {
+//            CvScalar s = cvGet2D(src, row, col);
+//            cvSet2D(destImg, row, col, s);
+//        }
+//    }
+//
+//    return destImg;
+//}
--- app/src/main/java/com/findai/xkk/wearglass/MainActivity.java
+++ app/src/main/java/com/findai/xkk/wearglass/MainActivity.java
@@ -1,8 +1,16 @@
 package com.findai.xkk.wearglass;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +19,30 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    ImageView qian;
+    ImageView hou;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
+        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.mipmap.eye)).getBitmap();
+
+//        int w = bitmap.getWidth();
+//        int h=bitmap.getHeight();
+//        int[] pixels = new int[w*h];
+//        bitmap.getPixels(pixels,0,w,0,0,w,h);
+        qian = findViewById(R.id.qian);
+        hou = findViewById(R.id.hou);
+        qian.setImageBitmap(bitmap);
+        long s = System.currentTimeMillis();
+
+        Bitmap bitmap1 = process_img(bitmap);
+        System.out.println(System.currentTimeMillis() - s);
+        hou.setImageBitmap(bitmap1);
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText("反而");
     }
 
     /**
@@ -26,4 +50,67 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public Bitmap process_img(Bitmap bitmap) {
+        Mat mat = new Mat();
+        Utils.bitmapToMat(bitmap, mat);
+//        System.out.println("1111111111111");
+//        System.out.println(mat);
+        Mat resize_mat = new Mat();
+        long s = System.currentTimeMillis();
+        Imgproc.resize(mat, resize_mat, new Size(mat.cols() / 6, mat.rows() / 6), 0, 0, Imgproc.INTER_NEAREST);
+
+        System.out.println(System.currentTimeMillis() - s);
+//        String proc_pixels = stringFromJNI(pixels,w,h);
+//        bitmap.getPixels(   )
+        Bitmap bmp = Bitmap.createBitmap(resize_mat.cols(), resize_mat.rows(), Bitmap.Config.RGB_565);
+        Mat gray_mat = new Mat();
+        Imgproc.cvtColor(resize_mat, gray_mat, Imgproc.COLOR_BGRA2GRAY, 1);
+//        Bitmap bsize = new Bitmap();
+//        Utils.matToBitmap(graymat,bmp);
+        // Example of a call to a native method
+
+        double global_max = 0;
+        double[][] sumimg = new double[mat.rows() / 6][mat.cols() / 6];
+
+        for (int i = 10; i < gray_mat.rows() - 10; i = i + 3) {
+            for (int j = 10; j < gray_mat.cols() - 10; j = j + 3) {
+                double sum = 0;
+                int ci = i;
+                for (int k = 0; k < 5; k++) {
+//                    System.out.println(graymat.get(k,j).length);
+                    sum = sum + (gray_mat.get(ci + k, j)[0] - gray_mat.get(ci - k, j)[0]);
+//                    System.out.println(sum);
+                }
+//                new_mat.put(i,j,sum);
+                sumimg[i][j] = sum;
+                if (global_max < sum) {
+                    global_max = sum;
+                }
+//                System.out.println();
+            }
+        }
+
+        for (int j = 10; j < gray_mat.cols() - 10; j = j + 3) {
+            double max = global_max * 0.5;
+            int max_i = 0;
+            int max_j = 0;
+//                System.out.println();
+            for (int i = 10; i < gray_mat.rows() - 10; i = i + 3) {
+                if (max < sumimg[i][j]) {
+                    max = sumimg[i][j];
+//                    System.out.println(max);
+                    max_i = i;
+                    max_j = j;
+                }
+            }
+//            System.out.println("I:"+max_i+"----J:"+max_j+"----max:"+max);
+            double[] sz = new double[]{255};
+            gray_mat.put(max_i, max_j, sz);
+        }
+//        System.out.println(gray_mat.cols()+"  "+gray_mat.rows());
+//        System.out.println(System.currentTimeMillis()-s);
+        Utils.matToBitmap(gray_mat, bmp);
+        return bmp;
+    }
 }
